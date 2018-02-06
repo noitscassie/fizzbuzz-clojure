2dee1d8cc8a150360694822f98759b11606b0550
Peter Johnstone <peter.wdj@gmail.com>
2018-02-06 10:48:54 +0000

    Add additional test values

--- test/fizzbuzz/fizzbuzz_test.clj
+++ test/fizzbuzz/fizzbuzz_test.clj
@@ -7,8 +7,10 @@
   (fact "it returns 'fizz' for multiples of 3"
     (fizzbuzz 3) => "fizz"
     (fizzbuzz 6) => "fizz"
+    (fizzbuzz 9) => "fizz"
   )
   (fact "it returns 'buzz' for multiples of 5"
     (fizzbuzz 5) => "buzz"
+    (fizzbuzz 10) => "buzz"
   )
 )
